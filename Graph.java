88cf3427de5dafc2173d21a05d8fb8b6eb97e601
bowenz35 <36973770+bowenz35@users.noreply.github.com>
2018-04-12 17:43:32 -0500

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,27 +1,33 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+
+import java.util.*;
+
 
 /**
  * Undirected and unweighted graph implementation
- * 
+ *
  * @param <E> type of a vertex
- * 
+ *
  * @author sapan (sapan@cs.wisc.edu)
- * 
+ *
  */
 public class Graph<E> implements GraphADT<E> {
-    
+
     /**
      * Instance variables and constructors
      */
+    HashMap<E, ArrayList<E>> dictionary = new HashMap<>();
 
     /**
      * {@inheritDoc}
      */
     @Override
     public E addVertex(E vertex) {
-        
+        if (vertex == null || dictionary.containsKey(vertex)) {
+            return null;
+        } else {
+            dictionary.put(vertex, new ArrayList<>());
+            return vertex;
+        }
     }
 
     /**
@@ -29,7 +35,15 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public E removeVertex(E vertex) {
-        
+        if (vertex == null || !dictionary.containsKey(vertex)) {
+            return null;
+        } else {
+            for (E opposite : getNeighbors(vertex)) {
+                dictionary.get(opposite).remove(vertex);
+            }
+            dictionary.remove(vertex);
+            return vertex;
+        }
     }
 
     /**
@@ -37,15 +51,38 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean addEdge(E vertex1, E vertex2) {
-        
-    }    
+        if (vertex1 != null
+                && vertex2 != null
+                && vertex1 != vertex2
+                && dictionary.containsKey(vertex1)
+                && dictionary.containsKey(vertex2)) {
+            dictionary.get(vertex1).add(vertex2);
+            dictionary.get(vertex2).add(vertex1);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /**
      * {@inheritDoc}
      */
     @Override
     public boolean removeEdge(E vertex1, E vertex2) {
-        
+        if (vertex1 != null
+                && vertex2 != null
+                && vertex1 != vertex2
+                && dictionary.containsKey(vertex1)
+                && dictionary.containsKey(vertex2)
+                && dictionary.get(vertex1).contains(vertex2)
+                && dictionary.get(vertex2).contains(vertex1)) {
+            dictionary.get(vertex1).remove(vertex2);
+            dictionary.get(vertex2).remove(vertex1);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     /**
@@ -53,7 +90,15 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean isAdjacent(E vertex1, E vertex2) {
-        
+        if (vertex1 != null
+                && vertex2 != null
+                && vertex1 != vertex2
+                && dictionary.containsKey(vertex1)
+                && dictionary.containsKey(vertex2)) {
+            return dictionary.get(vertex1).contains(vertex2) && dictionary.get(vertex2).contains(vertex1);
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -61,7 +106,11 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public Iterable<E> getNeighbors(E vertex) {
-        
+        if (vertex != null && dictionary.containsKey(vertex)) {
+            return dictionary.get(vertex);
+        } else {
+            return null;
+        }
     }
 
     /**
@@ -69,7 +118,6 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public Iterable<E> getAllVertices() {
-        
+        return dictionary.keySet();
     }
-
 }
