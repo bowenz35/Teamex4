2a8018fee708ef5976424b6088d018bebfdc0e08
bowenz35 <36973770+bowenz35@users.noreply.github.com>
2018-04-16 17:03:47 -0500

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,4 +1,13 @@
-
+///////////////////////////////////////////////////////////////////////////////
+// assignment name: p4
+// Author: Bowen Zhang
+// Partner:GRIFF ZHANG JICHEN ZHANG JUNGE ZHANG Tianyuan(Rainer) Yuan
+// Email : bzhang296@wisc.edu
+// due date: April 15th 2018
+// CS Login: bowenz
+// Credits: none
+// known bugs: none
+//////////////////////////////////////////////////////////////////////////////
 import java.util.*;
 
 
@@ -15,10 +24,23 @@ public class Graph<E> implements GraphADT<E> {
     /**
      * Instance variables and constructors
      */
-    HashMap<E, ArrayList<E>> dictionary = new HashMap<>();
-
+	
+  
+	private HashMap<E,ArrayList<E>>dictionary;
+	
+	/**
+	  * constructor for graph class
+	  */
+	public Graph() {
+		dictionary=new HashMap<>();
+	}
+   
+   
     /**
-     * {@inheritDoc}
+     * add vertex 
+     * parameter  E vertex 
+     * return null if duplicate or add null
+     * otherwise add to arraylist 
      */
     @Override
     public E addVertex(E vertex) {
@@ -31,7 +53,10 @@ public class Graph<E> implements GraphADT<E> {
     }
 
     /**
-     * {@inheritDoc}
+     * remove vertex
+     * parameter E vertex
+     * return null if vertex is null or arraylist does not have this vertex
+     * else remove vertex from array list
      */
     @Override
     public E removeVertex(E vertex) {
@@ -47,7 +72,10 @@ public class Graph<E> implements GraphADT<E> {
     }
 
     /**
-     * {@inheritDoc}
+     * add Edge
+     * parameter E vertex1,  E vertex2
+     * return true if both vertices exist and are in array list 
+     * else return false
      */
     @Override
     public boolean addEdge(E vertex1, E vertex2) {
@@ -65,7 +93,11 @@ public class Graph<E> implements GraphADT<E> {
     }
 
     /**
-     * {@inheritDoc}
+     * remove edge
+     * parameter E vertex1  E vertex2
+     * return true if both vertices exist and are in the array list
+     * and there is an edge between two vertices 
+     * else return false
      */
     @Override
     public boolean removeEdge(E vertex1, E vertex2) {
@@ -86,7 +118,10 @@ public class Graph<E> implements GraphADT<E> {
     }
 
     /**
-     * {@inheritDoc}
+     * Check whether the two vertices are adjacent
+     * parameter vertex1  vertex2 
+     * return true if both the vertices have an edge with each other, else return false 
+     * if vertex1 and vertex2 are not connected (also if valid conditions are violated)
      */
     @Override
     public boolean isAdjacent(E vertex1, E vertex2) {
@@ -102,7 +137,9 @@ public class Graph<E> implements GraphADT<E> {
     }
 
     /**
-     * {@inheritDoc}
+     * Get all the neighbor vertices of a vertex
+     * parameter vertex
+     * return  an iterable for all the immediate connected neighbor vertices
      */
     @Override
     public Iterable<E> getNeighbors(E vertex) {
@@ -114,7 +151,8 @@ public class Graph<E> implements GraphADT<E> {
     }
 
     /**
-     * {@inheritDoc}
+     * Get all the vertices in the graph
+     * @return an iterable for all the vertices
      */
     @Override
     public Iterable<E> getAllVertices() {
